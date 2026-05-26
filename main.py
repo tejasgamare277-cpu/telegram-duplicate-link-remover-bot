3ee2a99c539eb290dbb3a484b16a49ff26b4006d
tejasgamare277-cpu <tejasgamare277@gmail.com>
2026-05-26 18:42:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import re
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = 8992802234:AAE_89qVGU9KIc13a-GX8WLNRvTYPhEUMJ4
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 sent_links = set()
 
@@ -14,24 +15,16 @@ async def remove_duplicate_links(update: Update, context: ContextTypes.DEFAULT_T
 
     links = re.findall(r'(https?://\S+|t\.me/\S+)', text)
 
-    duplicate_found = False
-
     for link in links:
         if link in sent_links:
-            duplicate_found = True
             try:
                 await update.message.delete()
-                await update.message.reply_text(
-                    "⚠️ Duplicate link removed!"
-                )
-            except:
-                pass
+                await update.message.reply_text("⚠️ Duplicate link removed!")
+            except Exception as e:
+                print(e)
             return
         else:
             sent_links.add(link)
-
-    if not duplicate_found:
-        print("New link allowed")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
